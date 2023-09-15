a1a0cbf9bedcca5549a1e50335a39722016acc7e
Replit user <>
2023-09-15 00:34:17 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -49,7 +49,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 }
 
 
-voi insertTreeMap(TreeMap * tree, void* key, void * value) {
+voiD insertTreeMap(TreeMap * tree, void* key, void * value) {
   TreeNode* node = tree->root;
   TreeNode* parent = NULL;
    while (node != NULL) {
